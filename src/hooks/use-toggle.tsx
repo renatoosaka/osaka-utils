a8ba48190800b745217bb6b4baf73c8056a58fd7
Renato Hideki Osaka <renato.osaka@hotmail.com>
2023-03-15 21:25:02 -0300

    refactor: toggle hook

--- src/hooks/use-toggle.tsx
+++ src/hooks/use-toggle.tsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 
-export function useToggle<T>(initialValue: T, options: [T, T]): [T, () => void] {
-  const [state, setState] = useState(initialValue)
+type ToggleProps<T> = {
+  initialValue?: T,
+  options: [T, T],
+}
+export function useToggle<T>({ initialValue, options }: ToggleProps<T>): [T, () => void] {
+  const [state, setState] = useState(() => {
+    if (!initialValue) return options[0];
+
+    return initialValue;
+  })
 
   function toggle() {
     setState(state === options[0] ? options[1] : options[0])
